f9804a28b53d43d79248df0f38c98a814d97e56a
albingroen <albin.groen@gmail.com>
2021-07-30 22:17:21 +0200

    :lipstick: Empty component padding fix

--- components/empty.tsx
+++ components/empty.tsx
@@ -10,7 +10,7 @@ export default function Empty({ children }: IEmptyProps) {
   return (
     <View
       style={tailwind(
-        "p-12 rounded-lg border border-gray-300 justify-center items-center w-full"
+        "px-12 py-8 rounded-lg border border-gray-300 justify-center items-center w-full"
       )}
     >
       <Text style={tailwind("text-lg text-gray-500 text-center")}>
